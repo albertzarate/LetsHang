08c69b24679f0a7969f48943604da6a0b82a47bc
Albert Zarate <thealbertzarate@gmail.com>
2017-11-28 09:36:18 -0800

    more django set up

---
+++ LetsHang/views.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+#from django.views.generic import TemplateView
+'''
+class HomePage(TemplateView):
+    template_name = 'index.html'
+'''
+def index(request):
+    return render(request, 'LetsHang/index.html', {
+    'title': 'Welcome to the Home Page Boi'
+
+})
