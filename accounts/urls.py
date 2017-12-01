f542b057211f0ae23a4fec84ff8af29dc108736a
Albert Zarate <thealbertzarate@gmail.com>
2017-11-30 18:04:25 -0800

    using pinax

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.contrib.auth.views import login, logout
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
-]
